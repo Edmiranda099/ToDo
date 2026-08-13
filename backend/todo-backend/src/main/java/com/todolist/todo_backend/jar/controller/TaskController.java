876367786772f8868b9cd83286a382d7804671ae
Edmiranda099 <edmiranda099@gmail.com>
2026-08-13 17:11:09 -0300

    criar nossa primeira API REST em Spring Boot

---
+++ backend/todo-backend/src/main/java/com/todolist/todo_backend/jar/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.todolist.todo_backend.jar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //"Essa classe vai receber requisições HTTP e devolver respostas."
+
+public class TaskController {
+    
+    @GetMapping("/tasks") //Quando alguém fizer um GET para /tasks, execute o método abaixo.
+    public String getTasks(){
+        return "Minha lista de tarefas";
+    }
+
+}
